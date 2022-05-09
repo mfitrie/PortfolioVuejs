c77ce99deb8a2a8a7886ab01dee6a36888850bcc
mfitrie <mfitrie78@gmail.com>
2022-05-09 23:16:35 +0800

    adding loading animation

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 
 const portfolioBox = [...document.querySelectorAll('.section__two--portfolio div')];
 
+const greetings = document.querySelectorAll('.main__one--animated');
+const background__loader = document.querySelector('.background__loader');
+const loader = document.querySelector('.loader');
+
 const link = ['https://github.com/mfitrie/Natours', 'https://github.com/mfitrie/forkify-web-recipe', 'https://github.com/mfitrie/WaifuApp', 'https://github.com/mfitrie/iSpend', 'https://github.com/mfitrie/Car-Rental-Syetem', 'https://github.com/mfitrie/WebScrapper'];
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', function(e){
+    setTimeout(()=>{
+        background__loader.style.display = 'none';
+        loader.style.display = 'none';
+    
+        console.log('im ready');
+
+        anime({
+            targets: greetings,
+            translateX: 270
+        });
+
+    }, 1000);
+});
 
 
 
@@ -15,10 +38,5 @@ portfolioBox.forEach((el, i) =>{
 
 
 
-const greetings = document.querySelectorAll('.main__one--animated');
 
-anime({
-    targets: greetings,
-    translateX: 270
-});
 
